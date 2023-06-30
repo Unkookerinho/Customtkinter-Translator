5b00c141e0f6c130966d7dfb3ea65bd60c531e7a
Unkookerinho <Unkookerinho@gmail.com>
2023-06-30 08:54:46 +0200

    Language update
    Now you can choose between several languages for source and destination of your text. Improved readability.

--- README.md
+++ README.md
@@ -1,22 +1,43 @@
 # Customtkinter-Translator
-Language translator made in Python using customtkinter and googletrans libraries.<br>
-It basically translates a word and saves it in visible library as {source - destination}.<br>
-You can delete items from library by clicking them. Library is saved in json file, so it reloads previous translations.
+<hr>
+Language translator made in Python using customtkinter and googletrans libraries.
 
+## Features
+<hr>
+<ul>
+<li>Translating a text and saving it in visible library</li> 
+<li>Choosing between several languages</li> 
+<li>Deleting previous translations by clicking on them</li> 
+<li>Library is saved in json file, so it remembers your previous translations</li> 
+</ul>
+
+## Screenshots
+<hr>
 <p align="left"><img src="CtkTranslator.png"></p>
 
 ## Language
+<hr>
 Default source language is English and destination is Polish.<br>
-I didn't implement changing it in app, so you have to do this in code in translate section.
-```
-if to_translate:
-    # Default source is English and destination is Polish
-    # Use language codes like en/pl/de/fr in src="en", dest="pl"
-    translated = translator.translate(text=to_translate, src="en", dest="pl")
+You can change both of them between English(en), French(fr), Italian(it), German(de), Spanish(es) and Polish(pl).<br>
+If language you want is missing, you can add it to values in this part:
+
+```python
+
+self.source = ctk.CTkOptionMenu(master=self.frame2, width=125, corner_radius=0,
+                                values=["en", "fr", "it", "de", "es", "pl"])
+
+self.source.grid(row=1, column=0, padx=18)
+
+self.destination = ctk.CTkOptionMenu(master=self.frame2, width=125, corner_radius=0,
+                                     values=["pl", "es", "de", "it", "fr", "en"])
+self.destination.grid(row=1, column=1, padx=8)
 ```
 
+
 ## Python Pip
+<hr>
 <ul>
 <li>pip install customtkinter</li>
 <li>pip install googletrans==3.1.0a0</li>
 </ul>
+It needs to be that exact version of googletrans, because the newest one is not working.
--- main.py
+++ main.py
@@ -2,6 +2,7 @@ import json
 import customtkinter as ctk
 from googletrans import Translator
 
+# Preparing translator and library
 translator = Translator()
 library = {}
 
@@ -11,7 +12,6 @@ try:
         library = json.load(file)
 except:
     pass
-
 
 # Preparing previous cases
 keys = []
@@ -24,54 +24,81 @@ num = len(library)
 
 
 class TranslatorApp(ctk.CTk):
+    """Translator app that translates text from chosen source to chosen destination and saves it in library"""
+
     def __init__(self):
         super().__init__()
 
         # Root
-        self.geometry("350x700")
+        self.geometry("350x740")
         self.title("Customtkinter Translator")
+        self.resizable(False, False)
 
         # Appearance
         self.appearance = ctk.set_appearance_mode("dark")
         self.theme = ctk.set_default_color_theme("dark-blue")
-        # Frame
-        self.frame = ctk.CTkFrame(master=self)
-        self.frame.pack(pady=20, padx=20, fill="both", expand=True)
 
-        # Frame widgets
-        self.title = ctk.CTkLabel(master=self.frame, text="Ctk Translator", font=ctk.CTkFont(size=30, weight="bold"))
+        # Frames
+        self.frame1 = ctk.CTkFrame(master=self)
+        self.frame1.pack(pady=20, padx=20, fill="both")
+
+        self.frame2 = ctk.CTkFrame(master=self)
+        self.frame2.pack(padx=20, fill="x")
+
+        # Frame1 widgets
+        self.title = ctk.CTkLabel(master=self.frame1, text="Ctk Translator", font=ctk.CTkFont(size=30, weight="bold"))
         self.title.pack(pady=30)
 
-        self.library_frame = ctk.CTkScrollableFrame(master=self.frame, width=260, height=480, corner_radius=0)
+        self.library_frame = ctk.CTkScrollableFrame(master=self.frame1, width=265, height=480, corner_radius=0)
         self.library_frame.pack()
 
-        # Spawning previous cases
+        self.user_input = ctk.CTkEntry(master=self.frame1, width=285,
+                                       placeholder_text="Input text you want to translate",
+                                       corner_radius=0)
+        self.user_input.pack()
+        self.button = ctk.CTkButton(master=self.frame1, width=285, text="Enter", corner_radius=0,
+                                    command=lambda: translate())
+        self.button.pack()
+
+        # Frame2 widgets
+        self.source_title = ctk.CTkLabel(master=self.frame2, text="Source", font=ctk.CTkFont(size=13))
+        self.source_title.grid(row=0, column=0, padx=15)
+
+        self.destination_title = ctk.CTkLabel(master=self.frame2, text="Destination", font=ctk.CTkFont(size=13))
+        self.destination_title.grid(row=0, column=1, padx=10)
+        self.source = ctk.CTkOptionMenu(master=self.frame2, width=125, corner_radius=0,
+                                        values=["en", "fr", "it", "de", "es", "pl"])
+
+        self.source.grid(row=1, column=0, padx=18)
+
+        self.destination = ctk.CTkOptionMenu(master=self.frame2, width=125, corner_radius=0,
+                                             values=["pl", "es", "de", "it", "fr", "en"])
+        self.destination.grid(row=1, column=1, padx=8)
+
+        # Spawning previous translations
         for i in range(num):
             element = ctk.CTkButton(master=self.library_frame, width=200, text=(keys[i], "-", values[i]))
             element.configure(command=lambda button=element, txt=keys[i]: delete_self(button, txt))
             element.pack(pady=10)
 
-        self.user_input = ctk.CTkEntry(master=self.frame, width=280,
-                                       placeholder_text="Input text you want to translate",
-                                       corner_radius=0)
-        self.user_input.pack()
-        self.button = ctk.CTkButton(master=self.frame, width=280, text="Enter", corner_radius=0,
-                                    command=lambda: translate())
-        self.button.pack()
-
         def translate():
             """Function that translates input from user, adds it to library and saves it"""
-            # Translation, adding it to library
+
+            # Translation, avoiding translating blank input
             to_translate = self.user_input.get()
             if to_translate:
-                # Default source is English and destination is Polish
-                # Use language codes like en/pl/de/fr in src="en", dest="pl"
-                translated = translator.translate(text=to_translate, src="en", dest="pl")
-                library[to_translate] = translated.text
-
-                result = ctk.CTkButton(master=self.library_frame, width=200,
-                                       text=(self.user_input.get(), "-", translated.text))
-                result.configure(command=lambda button=result: delete_self(button, to_translate))
+                translated = translator.translate(text=to_translate, src=self.source.get(), dest=self.destination.get())
+                # Getting rid of spaces that causes curly braces to appear
+                to_translate = to_translate.replace(" ", " ")
+                translated.text = translated.text.replace(" ", " ")
+                source = "".join("(" + str(self.source.get()) + ") " + str(to_translate))
+                destination = "".join(str(translated.text) + " (" + str(self.destination.get()) + ")")
+                library[source] = destination
+                # Configuring result button
+                result_text = "(" + self.source.get() + ") " + to_translate, "-", \
+                              translated.text + " (" + self.destination.get() + ")"
+                result = ctk.CTkButton(master=self.library_frame, width=200, text=result_text)
+                result.configure(command=lambda button=result: delete_self(button, source))
                 result.pack(pady=10)
 
             # Saves previous cases
@@ -83,6 +110,7 @@ class TranslatorApp(ctk.CTk):
 
         def delete_self(button, text):
             """Deletes button with translation, deletes translation from library"""
+
             button.destroy()
             del library[text]
             with open("library.json", "w") as file:
